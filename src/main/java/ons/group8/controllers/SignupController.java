ba8476e0ea26a2e31ba53c10ca31d780250bd06b
Nimrod Shefi <shefin@cardiff.ac.uk>
2020-11-24 12:27:30 +0000

    starting to work on the sign up page

---
+++ src/main/java/ons/group8/controllers/SignupController.java
@@ -0,0 +1,8 @@
+package ons.group8.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SignupController {
+
+}
--- src/main/java/ons/group8/domain/User.java
+++ src/main/java/ons/group8/domain/User.java
@@ -2,12 +2,22 @@ package ons.group8.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     private String email;
     private String fname;
     private String lname;
