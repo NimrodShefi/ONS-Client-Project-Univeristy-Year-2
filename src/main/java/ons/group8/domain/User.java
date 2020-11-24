01f693712cbbfffa9a03ec2db8a99819cbc60716
c1946094 <ShahS58@cardiff.ac.uk>
2020-11-24 20:39:59 +0000

    added jdbc to application.properties

--- src/main/java/ons/group8/domain/User.java
+++ src/main/java/ons/group8/domain/User.java
@@ -16,4 +16,11 @@ public class User {
     private String lname;
     private String password;
     private List<String> roles;
+
+    public User(String theId, String theEmail, String thePassword, String theFname, String theLname){
+        this.id = null;
+        this.email = theEmail;
+        this.fname = theFname;
+        this.lname = theLname;
+    }
 }
