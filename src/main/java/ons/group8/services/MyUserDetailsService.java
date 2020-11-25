72020a971bd0aa42b71fafed051261c0a1f685a0
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-11-25 16:31:17 +0000

    Add MyUserDetailsService class with one method to load user by email.

---
+++ src/main/java/ons/group8/services/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package ons.group8.services;
+
+import ons.group8.config.MyUserDetails;
+import ons.group8.domain.User;
+import ons.group8.repositories.UserRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepositoryJPA userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.getUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        } else {
+            return new MyUserDetails(user);
+        }
+    }
+}
