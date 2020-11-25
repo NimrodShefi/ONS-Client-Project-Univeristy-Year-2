ef80e75a3e21b7875ffb22960e0a3862cdaf884a
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-11-25 15:25:52 +0000

    Add user repository with one method to find user by email.

---
+++ src/main/java/ons/group8/repositories/UserRepositoryJPA.java
@@ -0,0 +1,12 @@
+package ons.group8.repositories;
+
+import ons.group8.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepositoryJPA extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User getUserByEmail(@Param("email") String email);
+}
