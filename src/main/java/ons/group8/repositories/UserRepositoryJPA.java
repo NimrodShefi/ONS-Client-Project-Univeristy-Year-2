775acba5f03442cf697256434e8bd92d71b79cef
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-11-25 16:32:09 +0000

    Add @Repository annotation to the UserRepositoryJPA

--- src/main/java/ons/group8/repositories/UserRepositoryJPA.java
+++ src/main/java/ons/group8/repositories/UserRepositoryJPA.java
@@ -4,7 +4,9 @@ import ons.group8.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepositoryJPA extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email")
