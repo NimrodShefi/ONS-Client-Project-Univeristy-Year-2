7003b4cc6b44e17ff501fe9d40e07e74260453dd
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-11-30 16:53:52 +0000

    Changed navbar to authorise on role instead of authority.

--- src/main/java/ons/group8/config/MyUserDetails.java
+++ src/main/java/ons/group8/config/MyUserDetails.java
@@ -25,7 +25,7 @@ public class MyUserDetails implements UserDetails {
         Set<Role> roles = user.getRoles();
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         for (Role role : roles) {
-            authorities.add(new SimpleGrantedAuthority(role.getName()));
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
         }
         return authorities;
     }
