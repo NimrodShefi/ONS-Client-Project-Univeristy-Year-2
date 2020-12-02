80ffbc92b63c1a87127f674267e2b5c155a219cb
Mahruk Zulfiqar <zulfiqarm1@cardiff.ac.uk>
2020-12-02 12:54:34 +0000

    completed the feature after completed the error handling and merge conflicts after merging

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,63 +13,64 @@ CONSTRAINT email_unique UNIQUE (email))
     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS ROLE(
-                                   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                   name VARCHAR(255) NOT NULL,
-                                   PRIMARY KEY (id),
-                                   CONSTRAINT role_unique UNIQUE (name))
+
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT role_unique UNIQUE (name))
     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS USER_ROLE(
-                                        user_id INT UNSIGNED NOT NULL,
-                                        role_id INT UNSIGNED NOT NULL,
-                                        FOREIGN KEY (user_id) REFERENCES USER(id),
-                                        FOREIGN KEY (role_id) REFERENCES ROLE(id))
+    user_id INT UNSIGNED NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES USER(id),
+    FOREIGN KEY (role_id) REFERENCES ROLE(id))
     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS CHECKLIST_TEMPLATE(
-                                                 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                 user_id INT UNSIGNED NOT NULL, -- this will contain the id of the author who create the list
-                                                 list_name VARCHAR(255) NOT NULL,
-                                                 description LONGTEXT NOT NULL,
-                                                 PRIMARY KEY(id),
-                                                 FOREIGN KEY (user_id) REFERENCES USER(id))
-    ENGINE = InnoDB;
+     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     user_id INT UNSIGNED NOT NULL, -- this will contain the id of the author who create the list
+     list_name VARCHAR(255) NOT NULL,
+     description LONGTEXT NOT NULL,
+     PRIMARY KEY(id),
+     FOREIGN KEY (user_id) REFERENCES USER(id))
+     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS TOPIC(
-                                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                    checklist_template_id INT UNSIGNED NOT NULL,
-                                    topic_name VARCHAR(255) NOT NULL,
-                                    description LONGTEXT NOT NULL,
-                                    PRIMARY KEY(id),
-                                    FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE(id))
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    checklist_template_id INT UNSIGNED NOT NULL,
+    topic_name VARCHAR(255) NOT NULL,
+    description LONGTEXT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE(id))
     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS CHECKLIST_TEMPLATE_ITEM(
-                                                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                      topic_id INT UNSIGNED NOT NULL,
-                                                      description LONGTEXT NOT NULL,
-                                                      PRIMARY KEY(id),
-                                                      FOREIGN KEY (topic_id) REFERENCES TOPIC(id))
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    topic_id INT UNSIGNED NOT NULL,
+    description LONGTEXT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (topic_id) REFERENCES TOPIC(id))
     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS PERSONAL_CHECKLIST(
-                                                 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                                 user_id INT UNSIGNED NOT NULL,
-                                                 checklist_template_id INT UNSIGNED NOT NULL,
-                                                 date_assigned DATE NOT NULL,
-                                                 date_complete DATE NOT NULL,
-                                                 PRIMARY KEY(id),
-                                                 FOREIGN KEY (user_id) REFERENCES USER(id),
-                                                 FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE(id))
-    ENGINE = InnoDB;
+     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     user_id INT UNSIGNED NOT NULL,
+     checklist_template_id INT UNSIGNED NOT NULL,
+     date_assigned DATE NOT NULL,
+     date_complete DATE NOT NULL,
+     PRIMARY KEY(id),
+     FOREIGN KEY (user_id) REFERENCES USER(id),
+     FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE(id))
+     ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS CHECKLIST_ITEM(
-                                             id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                             personal_checklist_id INT UNSIGNED NOT NULL,
-                                             checklist_template_id INT UNSIGNED NOT NULL,
-                                             checked BOOLEAN NOT NULL, -- boolean will show as TINYINT(1)
-                                             date_checked DATE NOT NULL,
-                                             PRIMARY KEY(id),
-                                             FOREIGN KEY (personal_checklist_id) REFERENCES PERSONAL_CHECKLIST(id),
-                                             FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE_ITEM(id))
-    ENGINE = InnoDB;
+     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     personal_checklist_id INT UNSIGNED NOT NULL,
+     checklist_template_id INT UNSIGNED NOT NULL,
+     checked BOOLEAN NOT NULL, -- boolean will show as TINYINT(1)
+     date_checked DATE NOT NULL,
+     PRIMARY KEY(id),
+     FOREIGN KEY (personal_checklist_id) REFERENCES PERSONAL_CHECKLIST(id),
+     FOREIGN KEY (checklist_template_id) REFERENCES CHECKLIST_TEMPLATE_ITEM(id))
+     ENGINE = InnoDB;
