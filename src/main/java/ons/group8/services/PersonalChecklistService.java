db5364a734d1c62b636f8c9e13ecd43e5502c55c
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-12-06 13:36:48 +0000

    Move ChecklistItemService methods to PersonalChecklistService.

--- src/main/java/ons/group8/services/PersonalChecklistService.java
+++ src/main/java/ons/group8/services/PersonalChecklistService.java
@@ -1,9 +1,11 @@
 package ons.group8.services;
 
+import ons.group8.domain.ChecklistItem;
 import ons.group8.domain.PersonalChecklist;
-import ons.group8.domain.User;
 
 public interface PersonalChecklistService {
 
     PersonalChecklist findByUserId_Id(Long userId);
+
+    ChecklistItem findByPersonalChecklist_Id(Long personalCheckId);
 }
--- src/main/java/ons/group8/services/PersonalChecklistServiceImpl.java
+++ src/main/java/ons/group8/services/PersonalChecklistServiceImpl.java
@@ -1,7 +1,8 @@
 package ons.group8.services;
 
+import ons.group8.domain.ChecklistItem;
 import ons.group8.domain.PersonalChecklist;
-import ons.group8.domain.User;
+import ons.group8.repositories.ChecklistItemRepositoryJPA;
 import ons.group8.repositories.PersonalChecklistRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class PersonalChecklistServiceImpl implements PersonalChecklistService {
 
+    private final PersonalChecklistRepositoryJPA personalChecklistRepository;
+    private final ChecklistItemRepositoryJPA checklistItemRepository;
+
     @Autowired
-    private PersonalChecklistRepositoryJPA personalChecklistRepository;
+    public PersonalChecklistServiceImpl(PersonalChecklistRepositoryJPA aPersonalChecklistRepository,
+                                    ChecklistItemRepositoryJPA aChecklistItemRepository){
+        personalChecklistRepository = aPersonalChecklistRepository;
+        checklistItemRepository = aChecklistItemRepository;
+    }
 
     public PersonalChecklist findByUserId_Id(Long userId){
         return personalChecklistRepository.findByUserId_Id(userId);
     };
+
+    public ChecklistItem findByPersonalChecklist_Id(Long personalCheckId){
+        return checklistItemRepository.findByPersonalChecklist_Id(personalCheckId);
+    }
 }
