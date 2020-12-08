92ed70f6f1ddba0a28aaabf6d292a7b39627e7bc
Mahruk Zulfiqar <zulfiqarm1@cardiff.ac.uk>
2020-12-08 14:13:57 +0000

    Added query in the repository to update the login failed attempt

--- src/main/java/ons/group8/repositories/UserRepositoryJPA.java
+++ src/main/java/ons/group8/repositories/UserRepositoryJPA.java
@@ -16,6 +16,10 @@ public interface UserRepositoryJPA extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    @Query("UPDATE User u SET u.failedAttempt = ?1 WHERE u.email = ?2")
+    @Modifying
+    public void updateFailedAttempts(int failAttempts, String email);
+
     @Query(value = "INSERT INTO user_role(userId, roleId) VALUES(:userId, :roleId)", nativeQuery = true)
     void saveUserRole(@Param("userId") Long userId, @Param("roleId") Integer roleId);
 
