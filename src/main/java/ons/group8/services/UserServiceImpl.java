19d3bb2d10b081ee0de49519bf35e743d3a9c0a3
Mahruk Zulfiqar <zulfiqarm1@cardiff.ac.uk>
2020-12-08 14:16:07 +0000

    Updated the user service class with methods and constants

--- src/main/java/ons/group8/services/UserServiceImpl.java
+++ src/main/java/ons/group8/services/UserServiceImpl.java
@@ -89,6 +89,39 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    public void increaseFailedAttempts(User user) {
+        int newFailAttempts = user.getFailedAttempt() + 1;
+        userRepository.updateFailedAttempts(newFailAttempts, user.getEmail());
+    }
+
+    public void resetFailedAttempts(String email) {
+        userRepository.updateFailedAttempts(0, email);
+    }
+
+    public void lock(User user) {
+        user.setAccountNonLocked(false);
+        user.setLockTime(new Date());
+
+        userRepository.save(user);
+    }
+
+    public boolean unlockWhenTimeExpired(User user) {
+        long lockTimeInMillis = user.getLockTime().getTime();
+        long currentTimeInMillis = System.currentTimeMillis();
+
+        if (lockTimeInMillis + LOCK_TIME_DURATION < currentTimeInMillis) {
+            user.setAccountNonLocked(true);
+            user.setLockTime(null);
+            user.setFailedAttempt(0);
+
+            userRepository.save(user);
+
+            return true;
+        }
+
+        return false;
+    }
+
     @Override
     public User findByEmail(String email) {
         return userRepository.findUserByEmail(email);
