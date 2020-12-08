02bc9eebc8ad5bd51d71e9edfa7ce5750f9d08e1
Nimrod Shefi <shefin@cardiff.ac.uk>
2020-12-08 19:34:50 +0000

    remove the profiles and have an application.properties in main and test, and now trying to get it to work

--- src/test/java/ons/group8/controllers/SignupTests.java
+++ src/test/java/ons/group8/controllers/SignupTests.java
@@ -6,8 +6,8 @@ import ons.group8.services.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,15 +15,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@AutoConfigureMockMvc
 @SpringBootTest
-@WebMvcTest(SignupController.class)
+@AutoConfigureMockMvc
 public class SignupTests {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private UserServiceImpl userService;
 
     @Autowired
