d7869ba9d6325faa3381b5e3faf6d17f6aea3568
Sameer Shah <shahs58@cardiff.ac.uk>
2020-12-08 21:55:13 +0000

    Update src/main/java/ons/group8/domain/User.java

--- src/main/java/ons/group8/domain/User.java
+++ src/main/java/ons/group8/domain/User.java
@@ -37,7 +37,7 @@ public class User {
     @NotEmpty
     private String password;
 
-    @ManyToMany( fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.ALL fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
