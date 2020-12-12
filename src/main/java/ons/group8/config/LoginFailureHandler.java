91f37b16ba55edda397367af5085099b7b15f1c5
Hannah Bonnett <bonnetthf@cardiff.ac.uk>
2020-12-12 14:39:31 +0000

    Change login handlers from @Service to @Configuration

--- src/main/java/ons/group8/config/LoginFailureHandler.java
+++ src/main/java/ons/group8/config/LoginFailureHandler.java
@@ -4,16 +4,16 @@ import ons.group8.domain.User;
 import ons.group8.services.LockOutService;
 import ons.group8.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.stereotype.Service;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Service
+@Configuration
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Autowired
