aeec72b0a85851b1e2f4c8f1fba218e9c8d7ae66
Nimrod Shefi <shefin@cardiff.ac.uk>
2020-12-14 16:26:09 +0000

    working on scrollable table

---
+++ src/main/resources/static/js/scrollable-table.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.table').DataTable({
+        "scrollY": "20%",
+        "scrollCollapse": true,
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
