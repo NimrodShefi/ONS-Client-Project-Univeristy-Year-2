efcb9450a2bfbcf4c6b75c3e2206b488ca950815
Nimrod Shefi <shefin@cardiff.ac.uk>
2020-12-21 12:46:37 +0000

    upadting an existing branch with up to date code and then working on the tests for that branch

--- src/test/java/ons/group8/controllers/SignupTests.java
+++ src/test/java/ons/group8/controllers/SignupTests.java
@@ -2,6 +2,7 @@ package ons.group8.controllers;
 
 import ons.group8.domain.User;
 import ons.group8.repositories.UserRepositoryJPA;
+import ons.group8.services.UserCreationEvent;
 import ons.group8.services.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -32,14 +34,26 @@ public class SignupTests {
     public void shouldGetSignUpPage() throws Exception {
 
         this.mockMvc
-                .perform(get("/sign-up/add-user"))
+                .perform(get("/sign-up/register"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
     public void shouldAddUser() throws Exception {
-        User user = new User("shefin@cardiff.ac.uk", "Password1!", "Nimrod", "Shefi");
-        userRepository.save(user);
+        UserCreationEvent user = new UserCreationEvent("nimrodshefi@cardiff.ac.uk", "Nimrod", "Shefi", "Password1!", "Password1!");
+        userService.save(user);
+
+        User retrievedUser = userRepository.findUserByEmail("nimrodshefi@cardiff.ac.uk");
+        assertEquals("nimrodshefi@cardiff.ac.uk", retrievedUser.getEmail());
+    }
+
+    @Test
+    public void shouldFailToAddUser() throws Exception {
+        UserCreationEvent user = new UserCreationEvent("nimrodshefi@cardiff.ac.uk", "Nimrod", "Shefi", "Password1!", "Password1!");
+        userService.save(user);
+
+        User retrievedUser = userRepository.findUserByEmail("nimrodshefi@cardiff.ac.uk");
+        assertEquals("nimrodshefi@cardiff.ac.uk", retrievedUser.getEmail());
     }
 }
