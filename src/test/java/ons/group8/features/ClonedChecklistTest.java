db4f733785dd46c914479518ec72e95158cc7b3f
Nimrod Shefi <shefin@cardiff.ac.uk>
2021-01-13 16:59:43 +0000

    creating the cloned checklist test and cleaning the signup tests

---
+++ src/test/java/ons/group8/features/ClonedChecklistTest.java
@@ -0,0 +1,16 @@
+package ons.group8.features;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ClonedChecklistTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
